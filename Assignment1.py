1ee9eab29d8388f5eab2936eef6208849b837e5e
Telugu-Archana <archut119@gmail.com>
2025-11-07 15:43:06 +0530

    FileUpdated

---
+++ Assignment1.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[68]:
+
+
+print(10)
+
+
+# In[39]:
+
+
+print(5,10,15)
+
+
+# In[40]:
+
+
+Name="Archana"
+print(Name)
+
+
+# In[41]:
+
+
+fn="Telugu"
+ln="Archana"
+print(fn,ln)
+
+
+# In[42]:
+
+
+age=21
+print("I am",age,"years old.")
+
+
+# In[43]:
+
+
+N="Archana"
+City="Hyderabad"
+print("My Name is {} and I live in {}".format(N,City))
+
+
+# In[44]:
+
+
+a=10
+b=20
+c=30
+print(a,"|",b,"|",c)
+
+
+# In[45]:
+
+
+print("Python","Rocks!")
+
+
+# In[46]:
+
+
+n1=5
+n2=8
+n3=n1+n2
+sum=str(n3)
+print(sum,type(sum))
+
+
+# In[47]:
+
+
+n=input("Enter your name: ")
+print("Hello,",n)
+
+
+# In[48]:
+
+
+city="Mumbai"
+print(city)
+
+
+# In[49]:
+
+
+a=10
+b=20
+print(a,b)
+a,b=b,a
+print(a,b)
+
+
+# In[50]:
+
+
+x=5
+y=10
+z="Python"
+print(x,y,z)
+
+
+# In[51]:
+
+
+color1="Red"
+color2="Red"
+color3="Red"
+print(color1,color2,color3)
+
+
+# In[52]:
+
+
+b=3.14
+print(b,type(b))
+
+
+# In[53]:
+
+
+name="Archana"
+age=21
+print("My name is",name,"and I am",age,"years old.")
+
+
+# In[54]:
+
+
+a="5"
+b=3
+print(a*b)
+
+
+# In[55]:
+
+
+is_python_fun=True
+print(is_python_fun)
+
+
+# In[56]:
+
+
+x=10
+print(x)
+x+=5
+print(x)
+
+
+# In[57]:
+
+
+a,b,c=5,5,5
+print(a,b,c)
+b=10
+print(a,b,c)
+
+
+# In[58]:
+
+
+a,b,c,d,e=0,0,0,0,0
+print(a,b,c,d,e)
+b=5
+print(a,b,c,d,e)
+
+
+# In[59]:
+
+
+x=10
+print(x)
+del(x)
+print(x)
+
+
+# In[60]:
+
+
+x=5
+y=7
+sum=x+y
+print(sum)
+
+
+# In[61]:
+
+
+a=2
+b=3
+print(a,b)
+a,b=b,a
+print(a,b)
+
+
+# In[62]:
+
+
+greeting="Hello"
+print(greeting)
+
+
+# In[63]:
+
+
+b=5*8
+print(b)
+
+
+# In[64]:
+
+
+a,b=1,2
+print(a,b)
+
+
+# In[65]:
+
+
+a,b="Telugu","Archana"
+print(a,b)
+
+
+# In[66]:
+
+
+message="Good Morning"
+print(message)
+del(message)
+print(message)
+
+
+# In[67]:
+
+
+num=10
+print(num)
+num=num+5
+print(num)
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment2.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print(a+b)
+
+
+# In[2]:
+
+
+f=float(input("Enter a float value: "))
+print(f)
+
+
+# In[3]:
+
+
+age=int(input("Enter your age: "))
+print(100-age)
+
+
+# In[4]:
+
+
+fn=input("Enter first name: ")
+ln=input("Enter last name: ")
+print("Full name is",fn,ln)
+
+
+# In[5]:
+
+
+r=float(input("Enter the radius: "))
+area=3.14*(r**2)
+print("Area of circle is",area)
+
+
+# In[8]:
+
+
+a=int(input("Enter a number"))
+b=a*2
+print("The product is",b)
+
+
+# In[9]:
+
+
+exp=input("Enter an math expression: ")
+e=eval(exp)
+print("The result of math expression is",e)
+
+
+# In[10]:
+
+
+s=input("Enter a number: ")
+a=int(s)+10
+print(a)
+
+
+# In[11]:
+
+
+a=float(input("Enter a float value: "))
+b=int(a)
+print(a,b)
+
+
+# In[12]:
+
+
+name=input("Enter your name: ")
+print("Welcome",name)
+
+
+# In[13]:
+
+
+a=12
+b=5
+print("Sum of a and b is",(a+b))
+
+
+# In[14]:
+
+
+x=10
+y=3
+print("Multifly of x and y is",(x*y))
+print("Subtraction of x and y is",(x-y))
+
+
+# In[15]:
+
+
+x=True
+print(x,type(x))
+
+
+# In[16]:
+
+
+a=17
+b=4
+print("The remainder is",a%b)
+
+
+# In[18]:
+
+
+a=2
+b=3
+c=4
+result=(a*b*c)-c
+print("The result is",result)
+
+
+# In[19]:
+
+
+a,b,c=5,6,9
+num=str(a)+str(b)+str(c)
+print("The number is",num)
+
+
+# In[20]:
+
+
+a=10
+b=25
+print("The difference is",(a-b))
+
+
+# In[21]:
+
+
+n=99
+print(n+1)
+
+
+# In[22]:
+
+
+num=4
+result=(num**2)+1
+print("The result is",result)
+
+
+# In[23]:
+
+
+n=6
+result=n*10-5
+print("The result is",result)
+
+
+# In[24]:
+
+
+print("The addition of 1.234 and 2.456 is",(1.234+2.456))
+
+
+# In[25]:
+
+
+r=2.5
+area=3.14*(r**2)
+print("The area is",area)
+
+
+# In[27]:
+
+
+f=2.5
+n=1000
+print(f*n)
+
+
+# In[28]:
+
+
+a=2.5
+b=4
+print(a*b)
+
+
+# In[29]:
+
+
+a,b,c=2.5,3.5,7.0
+avg=(a+b+c)/3
+print("The average is",avg)
+
+
+# In[30]:
+
+
+a=5
+b=8.0
+print(a,type(a))
+print(b,type(b))
+
+
+# In[31]:
+
+
+n=4
+f=2.0
+print(n*f)
+
+
+# In[32]:
+
+
+num=5
+print(num,type(num))
+num=num/2
+print(num,type(num))
+
+
+# In[33]:
+
+
+x=20
+print("The bool value of x is",bool(x))
+
+
+# In[34]:
+
+
+x=0
+print("The bool value of x is",bool(x))
+
+
+# In[ ]:
+
+
+
+
